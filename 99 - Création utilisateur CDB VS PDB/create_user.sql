5e807635f0f67a53d7d7ac076c51ed8298ae8ab7
Passion de données <passiondedonnees@gmail.com>
2020-12-20 18:01:07 -0500

    Création utilisateur dans un CDB VS PDB

--- 99 - Création utilisateur CDB VS PDB/create_user.sql
+++ 99 - Création utilisateur CDB VS PDB/create_user.sql
@@ -1,4 +1,5 @@
 sqlplus sys as sysdba@xe
+-- Saisir le mot de passe de sys
 
 sho user
 sho con_name
@@ -7,10 +8,12 @@ create user c##cdbadmin identified by qwerty;
 grant connect to c##cdbadmin;
 
 conn c##cdbadmin@xe
+-- Saisir le mot de passe de c##cdbadmin
 
 sho user
 
 conn sys as sysdba@xe
+-- Saisir le mot de passe de sys
 
 sho pdbs
 
@@ -26,5 +29,6 @@ create user pdb1admin identified by azerty;
 grant connect to pdb1admin;
 
 conn pdb1admin@xe
+-- Saisir le mot de passe de pdb1admin
 
 sho user
--- 99 - Création utilisateur CDB VS PDB/ouvrir_session.sql
+++ 99 - Création utilisateur CDB VS PDB/ouvrir_session.sql
@@ -1,7 +1,9 @@
 sqlplus sys as sysdba@localhost:1521/xe
+-- Saisir le mot de passe de sys
 
 
 sqlplus sys as sysdba@xe
+-- Saisir le mot de passe de sys
 
 sho user
 sho con_name
